6d350a1dabc5cd12d3b2e0e477317ac115f64ece
ManuelCorera <103545239+ManuelCorera@users.noreply.github.com>
2022-09-05 19:47:50 -0300

    Add files via upload

---
+++ stock.js
@@ -0,0 +1,6 @@
+let stockProductos = [
+    {id: 1, nombre: "GUITARRA", cantidad: 1, desc: "Clases de Guitarra", precio: 12000, img:"./imagenes/clasesguitarra.jpg"},
+    {id: 2, nombre: "PIANO", cantidad: 1, desc: "Clases de Piano", precio: 12000, img:"./imagenes/clasespiano.jpg"},
+    {id: 3, nombre: "CANTO", cantidad: 1, desc: "Clases de Canto", precio: 15000, img:"./imagenes/clasescanto.jpg"},
+    {id: 4, nombre: "BAJO", cantidad: 1, desc: "Clases de Bajo", precio: 14000, img:"./imagenes/bajo2.jpg"},
+]
